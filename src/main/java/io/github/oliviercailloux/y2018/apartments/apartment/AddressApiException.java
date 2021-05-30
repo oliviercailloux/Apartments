bb23c6070e0ef3900a81d7a7921bb69a42f06a52
MairameNdiaye <ndiaye.mairame@hotmail.com>
2021-05-30 12:41:46 +0200

    test formattage

--- src/main/java/io/github/oliviercailloux/y2018/apartments/apartment/AddressApiException.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/apartment/AddressApiException.java
@@ -19,14 +19,15 @@ public class AddressApiException extends Exception {
   /**
    * Constructs a new exception with the specified detail message and cause.
    *
-   * <p>Note that the detail message associated with <code>cause</code> is <i>not</i> automatically
+   * <p>
+   * Note that the detail message associated with <code>cause</code> is <i>not</i> automatically
    * incorporated in this exception's detail message.
    *
-   * @param message the detail message (which is saved for later retrieval by the {@link
-   *     Throwable#getMessage()} method).
+   * @param message the detail message (which is saved for later retrieval by the
+   *        {@link Throwable#getMessage()} method).
    * @param cause the cause (which is saved for later retrieval by the {@link Throwable#getCause()}
-   *     method). (A {@code null} value is permitted, and indicates that the cause is nonexistent or
-   *     unknown.)
+   *        method). (A {@code null} value is permitted, and indicates that the cause is nonexistent
+   *        or unknown.)
    */
   public AddressApiException(String message, Throwable cause) {
     super(message, cause);
