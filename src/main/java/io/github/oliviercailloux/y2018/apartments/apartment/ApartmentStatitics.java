a4fe3efcd014f62bba7ba71b5a6b005546524612
stellanweadau <stellan.wea@gmail.com>
2021-05-31 19:34:28 +0200

    Guava

--- src/main/java/io/github/oliviercailloux/y2018/apartments/apartment/ApartmentStatitics.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/apartment/ApartmentStatitics.java
@@ -11,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.math.DoubleMath;
+import com.google.common.math.Stats;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
 import io.github.oliviercailloux.y2018.apartments.apartment.json.JsonConvert;
@@ -29,127 +32,102 @@ public class ApartmentStatitics {
 
   /**
    * <p>
-   * Create a Map able to store the name of a criteria and its statistics.
+   * Create a Map able to store the name of a criteria and its data in a ArrayList.
    * </p>
    * <p>
    * <em>Note </em>: the first character of the criteria must be in lower case.
    * </p>
    * 
    * @param featureName : The name of the criteria thats we want to extract statistics.
-   * @return A Map (size 1) that has the name of the feature as its entry, and its statistics
-   *         through a DescriptiveStatistics as values of the Map.
+   * @return A Map (size 1) that has the name of the feature as its entry, and its data
+   *         through a ArrayList as values of the Map.
    */
 
-  public static HashMap<String, DescriptiveStatistics>
-      generateNumericStatitics(String featureName) {
+  public static HashMap<String, ArrayList<Double>> getNumericData(String featureName) {
 
     checkArgument(listOfFeatures.contains(featureName));
     LOGGER.info("{} is part of our criteria", featureName);
 
-    HashMap<String, DescriptiveStatistics> criteriaStats = new HashMap<>(1);
+    HashMap<String, ArrayList<Double>> criteriaStats = new HashMap<>(1);
 
     switch (featureName) {
 
       case "floorArea": {
-        List<Double> floorAreaStats = new ArrayList<>();
+        ArrayList<Double> floorAreaStats = new ArrayList<>();
         for (int i = 0; i < listOfApartments.size(); i++) {
           floorAreaStats.add(Math.round(listOfApartments.get(i).getFloorArea() * 100.0) / 100.0);
         }
-        criteriaStats.put(featureName, getNumericStatistics(floorAreaStats));
+        criteriaStats.put(featureName, floorAreaStats);
         LOGGER.info("Floor area statistics are available :\n");
         return criteriaStats;
       }
 
       case "floorAreaTerrace": {
-        List<Double> floorAreaTerraceStats = new ArrayList<>();
+        ArrayList<Double> floorAreaTerraceStats = new ArrayList<>();
         for (int i = 0; i < listOfApartments.size(); i++) {
           floorAreaTerraceStats
               .add(Math.round(listOfApartments.get(i).getFloorAreaTerrace() * 100.0) / 100.0);
         }
-        criteriaStats.put(featureName, getNumericStatistics(floorAreaTerraceStats));
+        criteriaStats.put(featureName, floorAreaTerraceStats);
         LOGGER.info("Floor area terrace statistics are available :\n");
         return criteriaStats;
       }
 
       case "nbBathrooms": {
-        List<Double> nbBathroomsStats = new ArrayList<>();
+        ArrayList<Double> nbBathroomsStats = new ArrayList<>();
         for (int i = 0; i < listOfApartments.size(); i++) {
           nbBathroomsStats.add(Double.valueOf(listOfApartments.get(i).getNbBathrooms()));
         }
-        criteriaStats.put(featureName, getNumericStatistics(nbBathroomsStats));
+        criteriaStats.put(featureName, nbBathroomsStats);
         LOGGER.info("Floor area terrace statistics are available :\n");
         return criteriaStats;
       }
 
       case "nbBedrooms": {
-        List<Double> nbBedroomsStats = new ArrayList<>();
+        ArrayList<Double> nbBedroomsStats = new ArrayList<>();
         for (int i = 0; i < listOfApartments.size(); i++) {
           nbBedroomsStats.add(Double.valueOf(listOfApartments.get(i).getNbBedrooms()));
         }
-        criteriaStats.put(featureName, getNumericStatistics(nbBedroomsStats));
+        criteriaStats.put(featureName, nbBedroomsStats);
         LOGGER.info("Number of bedrooms statistics are available  :\n");
         return criteriaStats;
       }
 
       case "nbMinNight": {
-        List<Double> nbMinNightStats = new ArrayList<>();
+        ArrayList<Double> nbMinNightStats = new ArrayList<>();
         for (int i = 0; i < listOfApartments.size(); i++) {
           nbMinNightStats.add(Double.valueOf(listOfApartments.get(i).getNbMinNight()));
         }
         LOGGER.info("The requested minimum number of night statistics are available :\n");
-        criteriaStats.put(featureName, getNumericStatistics(nbMinNightStats));
+        criteriaStats.put(featureName, nbMinNightStats);
         return criteriaStats;
       }
 
       case "nbSleeping": {
-        List<Double> nbSleepingStats = new ArrayList<>();
+        ArrayList<Double> nbSleepingStats = new ArrayList<>();
         for (int i = 0; i < listOfApartments.size(); i++) {
           nbSleepingStats.add(Double.valueOf(listOfApartments.get(i).getNbSleeping()));
         }
-        criteriaStats.put(featureName, getNumericStatistics(nbSleepingStats));
+        criteriaStats.put(featureName, nbSleepingStats);
         LOGGER.info("The number of sleepings statistics are available :\n");
         return criteriaStats;
       }
 
       case "pricePerNight": {
-        List<Double> pricePerNightStats = new ArrayList<>();
+        ArrayList<Double> pricePerNightStats = new ArrayList<>();
         for (int i = 0; i < listOfApartments.size(); i++) {
           pricePerNightStats
               .add(Math.round(listOfApartments.get(i).getPricePerNight() * 100.0) / 100.0);
         }
-        criteriaStats.put(featureName, getNumericStatistics(pricePerNightStats));
+        criteriaStats.put(featureName, pricePerNightStats);
         LOGGER.info("The price per night statistics are available :\n");
         return criteriaStats;
       }
 
       default:
-        LOGGER.info("\n{} isn't a Apartment feature", featureName);
-        return null;
+        throw new IllegalArgumentException(featureName+" isn't a Apartment feature");
     }
 
-  }
-
-  /**
-   * <p>
-   * Create a DescriptiveStatistics able to propose severals statistics about the double data
-   * provided in parameter.
-   * </p>
-   * <p>
-   * More information about this library in :
-   * <em>https://www.concretepage.com/java/jdk-8/java-8-summary-statistics-example</em>.
-   * </p>
-   * 
-   * @param listOfDoubles : a list of double
-   */
-
-  private static DescriptiveStatistics getNumericStatistics(List<Double> listOfDoubles) {
-
-    DescriptiveStatistics stats = new DescriptiveStatistics();
-    Iterator<Double> ite = listOfDoubles.listIterator();
-    while (ite.hasNext()) {
-      stats.addValue(ite.next());
-    }
-    return stats;
   }
 
   /**
@@ -157,9 +135,9 @@ public class ApartmentStatitics {
    * Create the Map that counts the occurrence of each boolean
    * </p>
    * 
-   * @param featureName : The name of the criteria thats we want to extract statistics.
+   * @param featureName : The name of the criteria thats we want to extract data.
    */
-  public static ImmutableMap<Boolean, Integer> generateBooleanStatitics(String featureName) {
+  public static ImmutableMap<Boolean, Integer> getBooleanData(String featureName) {
 
     checkArgument(listOfFeatures.contains(featureName));
     LOGGER.info("{} is part of our criteria", featureName);
@@ -181,6 +159,7 @@ public class ApartmentStatitics {
         LOGGER.info("The tele presence statistics are available :\n");
       }
         break;
+      
       case "terrace": {
         for (int i = 0; i < listOfApartments.size(); i++) {
           if (listOfApartments.get(i).getTerrace()) {
@@ -192,6 +171,7 @@ public class ApartmentStatitics {
         LOGGER.info("The terrace presence statistics are available :\n");
       }
         break;
+      
       case "wifi": {
         for (int i = 0; i < listOfApartments.size(); i++) {
           if (listOfApartments.get(i).getTerrace()) {
@@ -202,7 +182,11 @@ public class ApartmentStatitics {
         }
         LOGGER.info("The wifi presence statistics are available :\n");
       }
+        break;
 
+      default:
+        throw new IllegalArgumentException(featureName+" isn't a Apartment feature");
+    
     }
     results.put(true, yes);
     results.put(false, no);
@@ -212,27 +196,29 @@ public class ApartmentStatitics {
 
   /**
    * <p>
-   * Return the following statistics : the number of instances, its minimum, average and maximum.
+   * Return the following statistics : the number of instances, its minimum, average, maximum and standard deviation.
    * </p>
    * 
    * @param A Map (size 1) that has the name of the feature as its entry, and its statistics through
-   *        a DescriptiveStatistics as values of the Map.
+   *        a ArrayList as values of the Map.
    * 
    * @return a short string describing this instance.
    */
-  public static String displayNumericStatistics(HashMap<String, DescriptiveStatistics> dataMap) {
+  public static String displayNumericStatistics(HashMap<String, ArrayList<Double>> dataMap) {
 
-    Entry<String, DescriptiveStatistics> firstEntry = dataMap.entrySet().iterator().next();
+    Entry<String, ArrayList<Double>> firstEntry = dataMap.entrySet().iterator().next();
     String feature = firstEntry.getKey();
-    DescriptiveStatistics dss = firstEntry.getValue();
+    List<Double> listOfDoubles = firstEntry.getValue();
+    Stats stats = Stats.of(listOfDoubles);
+
     LOGGER.info("\n\nSome statistics about {} :", feature);
 
-    return ("\nNumber of apartments : " + dss.getN() + "\nMinimum : " + dss.getMin()
-        + "\nFirst quartile : " + Math.round(dss.getPercentile(25) * 100) / 100d + "\nMedian : "
-        + Math.round(dss.getPercentile(50) * 100) / 100d + "\nThird quartile : "
-        + Math.round(dss.getPercentile(75) * 100) / 100d + "\nMaximum : " + dss.getMax()
-        + "\nAverage : " + Math.round(dss.getMean() * 100) / 100d + "\nStandard deviation : "
-        + Math.round(dss.getStandardDeviation() * 100) / 100d + "\n");
+    return ("\nNumber of apartments : " + stats.count() + 
+            "\nMinimum : " + stats.min()+
+            "\nMaximum : " + stats.max()+
+            "\nAverage : " + Math.round(stats.mean() * 100) / 100d +
+            "\nStandard deviation : "+Math.round(stats.populationStandardDeviation() * 100) / 100d +
+            "\n");
   }
 
   /**
@@ -252,15 +238,17 @@ public class ApartmentStatitics {
 
   public static void main(String[] args) {
 
-    HashMap<String, DescriptiveStatistics> nbBedroomsStats = generateNumericStatitics("nbBedrooms");
-    ImmutableMap<Boolean, Integer> teleStats = generateBooleanStatitics("tele");
+    HashMap<String, ArrayList<Double>> nbBedroomsStats = getNumericData("nbBedrooms");
+    ImmutableMap<Boolean, Integer> teleStats = getBooleanData("tele");
 
     System.out.println(displayBooleanStatistics(teleStats));
     System.out.println(displayNumericStatistics(nbBedroomsStats));
 
-    System.out.println(displayNumericStatistics(generateNumericStatitics("nbMinNight")));
-    System.out.println(displayNumericStatistics(generateNumericStatitics("floorArea")));
-    System.out.println(displayBooleanStatistics(generateBooleanStatitics("wifi")));
+    System.out.println(displayNumericStatistics(getNumericData("nbMinNight")));
+    System.out.println(displayNumericStatistics(getNumericData("pricePerNight")));
+    System.out.println(displayBooleanStatistics(getBooleanData("wifi")));
 
+    // https://www.codota.com/code/java/classes/com.google.common.math.Stats
+    // https://vimsky.com/examples/detail/java-method-com.google.common.math.Stats.of.html
   }
 }
